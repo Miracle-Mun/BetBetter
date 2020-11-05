d1afb05350a18d57fe173a68c194dfd6e26fdf55
MiracleMMM <55248909+Miracle-Mun@users.noreply.github.com>
2020-11-05 01:22:41 -0800

    upload last UI

--- src/store/reducers/usersReducer.js
+++ src/store/reducers/usersReducer.js
@@ -1,21 +1,7 @@
 import * as actionTypes from '../actions/actionTypes'
-import jwt from 'jsonwebtoken';
-
-const validCredentials = () => {
-    const authorizationToken = localStorage.getItem('jwtToken');
-    if (authorizationToken === null)
-        return false;
-    try {
-        jwt.decode(authorizationToken);
-        return true;
-    } catch(err) {
-        return false;
-    }
-}
 
 const initialState = {
-    isAuthenticated: validCredentials(),
-    authenticatedUsername: validCredentials() === false ? '' : jwt.decode(localStorage.getItem('jwtToken')).username
+    isAuthenticated : false,
 };
 
 const reducer = (state = initialState, action) => {
